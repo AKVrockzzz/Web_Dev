b2d87e1cc69d4bd0eda460a241d5cc89a2437d43
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2024-12-31 19:26:22 +0530

    Html CSS JavaScript ()
    Html CSS JavaScript (Learning Phase)

---
+++ HTML_&_CSS_&_JavaScript/Day_18_JS_Intro/script.js
@@ -0,0 +1,14 @@
+alert("I this its their any problem");
+
+console.log("Code is running........");
+console.log("Code is also running........");
+console.log("Code is looking like a woww.........");
+
+var a = prompt("Enter your number: ");
+console.log("Your number is: " + a);
+
+
+var isTrue = confirm("Are your sure your want to blast your computer......");
+
+if (isTrue) console.log("Computer is blast");
+else console.log("Ha ha ha :-) Are you Scared.....");
---
+++ HTML_&_CSS_&_JavaScript/Day_19_JS_Varible_DataTypes/script.js
@@ -0,0 +1,35 @@
+console.log("Variables & Data Types");
+
+// var age = 25;
+
+// if(true){
+//     console.log(age);
+// }
+
+// function solve() {
+//     var name = "Ajay";
+//     console.log(name);
+// }
+
+// solve();
+
+// {
+//     let a = 20;
+//     console.log(a);
+// }
+
+// Reassigned
+
+// let a = 20;
+// a = 30;
+
+
+// Dynamicly Types language
+
+// let a = 10;
+//  a = "Verma";
+
+// const a = 20;
+// console.log(a);
+
+// a = 30; 
