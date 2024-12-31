fb996cd4baffcb04c4678ec7b4ff0a50b2558462
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2024-12-31 22:43:36 +0530

    JavaScript
    Condition & Operators

---
+++ HTML_&_CSS_&_JavaScript/Day_21_JS_Operators_&_Conditional/script.js
@@ -0,0 +1,60 @@
+// console.log("Verma Ji");
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+
+
+// let a = 5;
+
+// console.log(a++);
+// console.log(a);
+
+// console.log(++a);
+
+// let a = 5;
+
+// console.log(a--);
+ // console.log(a);
+
+// console.log(--a);
+
+// console.log('5' == 5);
+// console.log('5' === 5);
+
+// let age = 25;
+// let status1 = (age > 18) ? "I can vote" : "I can not vote";
+// console.log(status1);
+
+// if(age > 18){
+//     console.log("I can vote");
+// }
+// else {
+//     console.log("I can not vote");
+// }
+
+let num = 13;
+
+switch (num) {
+    case 1: console.log('A');
+            break;
+            case 2: console.log('B');
+            break;
+
+            case 3: console.log('C');
+            break;
+
+            case 4: console.log('D');
+            break;
+
+            case 5: console.log('E');
+            break;
+
+            default : console.log("!Invalid number");
+}
