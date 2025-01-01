e29497a6c8f1679999bf66dd8b3333aa3a0a301b
AKVrockzzz <ajaykumarverma5549@gmail.com>
2025-01-01 15:47:47 +0530

    JavaScript (Loops & Strings)
    JavaScript (Loops & Strings)

---
+++ HTML_&_CSS_&_JavaScript/Day_22_JS_Loops_&_Strings/script.js
@@ -0,0 +1,90 @@
+console.log("Loops and Strings");
+
+// let num = 1;
+// for(let i=0; i<5; i++){
+//     console.log(num);
+//     num++;
+// }
+
+// for(let i = 5; i>0 ; i--){
+//     console.log(i);
+// }
+
+
+// for(let i=1; i<=6; i++){
+//     if( i == 4) break;
+//     else console.log(i);
+// }
+
+
+// for(let i=1; i<=6; i++){
+//     if(i == 4) continue;
+//     else console.log(i);
+// }
+
+
+// let i = 1;
+// while(i <= 5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// let i = 5;
+// while(i > 0){
+//     console.log(i);
+//     i--;
+// }
+
+
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<= 10)
+
+
+
+// let i = 10;
+// do {
+//     console.log(i);
+//     i--;
+// }while(i > 0)
+
+// let firstName = "AKV";
+// let lastName = "Rockzzz";
+
+// console.log(firstName + lastName);
+
+
+// let name = `This
+// is
+// my
+// name
+// ajay`;
+
+// console.log(name);
+
+let opt1 = `English`;
+let opt2 = `Hindi`;
+
+let str = "Hlo ji kaise ho sare";
+
+
+console.log(str.split(' '));
+
+// console.log(opt1.substring(2,4));
+
+// console.log(opt2.toLowerCase());
+// console.log(opt1.toUpperCase());
+
+// console.log(opt2.length);
+// let ans = opt1 + opt2;
+
+// console.log(ans);
+
+
+// let finalAns = `${opt1} and ${opt2}`;
+// console.log(finalAns);
+
