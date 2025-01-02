6b1e1ebfc8b0ba271379c6385685c0ef100c2b93
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2025-01-02 22:20:47 +0530

    JavaScript(Function hoisting and function call) (Scoping and temporal dead zone)
    JavaScript(Function hoisting and function call) (Scoping and temporal dead zone)

---
+++ HTML_&_CSS_&_JavaScript/Day_25_JS_Function_Hosting_&_Calls/Script.js
@@ -0,0 +1,107 @@
+console.log("Function Hosting & Function Calls");
+
+// console.log(age);
+// var age = 25;
+
+
+// sayMyName("Akventures");
+// function sayMyName(fullName) {
+//     console.log(fullName);
+// }
+
+
+// console.log(num);
+// let num = 5;
+
+
+// console.log(num);
+// const num = 10;
+
+
+// sayHello();
+// let sayHello = function() {
+//     console.log("Hello ji kaise ho saare");
+
+// }
+
+
+
+// const Object = new Human();
+// class Human {
+
+// }
+
+
+
+// let greet = function () {
+//     console.log("Greeting of the day");
+// }
+
+// greet();
+
+// function greetMe(fullName){
+//     console.log("hello ", fullName);
+//     greet();
+// }
+
+// function greet() {
+//         console.log("Greeting of the day");
+// }
+
+// greetMe("Ajay");
+
+
+// function solve(number) {
+//     return function (number) {
+//         return number * number;
+//     }
+// }
+    
+// let ans = solve(5);
+// console.log(ans(10));
+
+
+
+// const arr = [
+//     function (a,b) {
+//         return a + b;
+//     },
+//     function (a,b) {
+//         return a - b;
+//     },
+//     function (a,b) {
+//         return a * b;
+//     },
+// ]
+
+// let first = arr[0];
+// console.log(first(10,5));
+
+// let second = arr[1];
+// console.log(second(10,5));
+
+// let third = arr[2];
+// console.log(third(10,5));
+
+
+
+// let obj = {
+//     age : 25,
+//     weight : 75,
+//     height : 180,
+//     greet: ()=> {
+//         console.log("Heelo Dunia");
+//     }
+// }
+
+// console.log(obj.age);
+// console.log(obj.weight);
+// console.log(obj.height);
+// obj.greet();
+
+
+// console.log(greet);
+// var greet = function(){
+//     console.log("namaste dunia");
+// }
+
---
+++ HTML_&_CSS_&_JavaScript/Day_26_JS_Temporal_Dead_Zone/Script.js
@@ -0,0 +1,87 @@
+console.log("JavaScript Temporal Dead Zone");
+
+// Temporal Dead Zone 
+// console.log(marks);
+// console.log("ajay");
+// console.log("vermaji");
+// let marks = 100;
+// console.log(marks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Scoping 
+
+// {
+//     const wt = 76;
+// }
+
+// console.log(wt);
+
+
+// {
+//     let ht = 150;
+// }
+// console.log(ht);
+
+
+
+
+
+
+
+// console.log(height);
+// {
+//     var height = 180;
+// }
+// console.log(height);
+
+
+
+
+
+
+
+
+
+
+
+
+// function sayHello(){
+//     var name1 = "Earth";
+//     console.log("Hello Duniya ", name1);
+// }
+
+// console.log(name1);
+// sayHello();
+
+
+
+
+
+
+//global scope
+
+// let age = 25;
+
+// console.log(age);
+
+// {
+//     console.log("block ", age);
+// }
+
+// function Age() {
+//     console.log("function", age);
+// }
+// Age();
