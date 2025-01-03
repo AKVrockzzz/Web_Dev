64d0ee1dda9fc33383ae9a1dfd7c3b7f19df149a
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2025-01-03 22:27:14 +0530

    JavaScript (Common In Built Object)
    JavaScript (Common In Built Object)

---
+++ HTML_&_CSS_&_JavaScript/Day_28_JavaScript_InBuilt_Objects/Script.js
@@ -0,0 +1,41 @@
+console.log("Common In Built Objects");
+
+let curr = new Date();
+console.log(curr);
+
+let date = new Date('June 20 1998 07:!5');
+console.log(date);
+
+let newDate = new Date(1998, 5, 20, 7);
+console.log(newDate);
+
+console.log(newDate.getFullYear());
+
+console.log(newDate.setDate(12));
+
+
+
+
+
+
+
+
+
+
+
+// console.log(Math.PI);
+
+// console.log(Math.max(600,30,40,500,708));
+// console.log(Math.min(600,30,40,500,708));
+
+// console.log(Math.round(1.6));
+// console.log(Math.round(1.4));
+
+// console.log(Math.floor(1.9));
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.abs(-6));
+
+// console.log(Math.sqrt(6));
+
+// console.log(Math.pow(2,10));
