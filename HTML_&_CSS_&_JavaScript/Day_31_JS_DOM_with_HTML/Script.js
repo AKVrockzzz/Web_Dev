5245a21bf1f0ad8ef11e5e10ba208ed8a811a869
AKVrockzzz <ajaykumarverma5549@gmail.com>
2025-01-08 13:33:35 +0530

    JavaScript DOM with html
    JavaScript DOM with html

---
+++ HTML_&_CSS_&_JavaScript/Day_31_JS_DOM_with_HTML/Script.js
@@ -0,0 +1,11 @@
+// let mydiv = document.querySelector('#mydiv');
+
+// let newElement = document.createElement('p');
+// newElement.textContent = "This is my new Element";
+
+// mydiv.insertAdjacentElement('beforebegin' , newElement);
+
+
+let parent = document.querySelector('#mydiv');
+let chile = document.querySelector('#child');
+parent.removeChild(child);
