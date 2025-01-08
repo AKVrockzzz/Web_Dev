432cabd4817c8d76681947f3a877ca0b0e10f6d9
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2025-01-08 22:06:10 +0530

    JavaScript DOM with CSS
    JavaScript DOM with CSS

---
+++ HTML_&_CSS_&_JavaScript/Day_32_JS_DOM_with_CSS/Script.js
@@ -0,0 +1,20 @@
+console.log("DOM with using CSS");
+
+// let fpara = document.querySelector('#fpara')
+// undefined
+// fpara
+// <p id=​"fpara" class=​"firstClass secondClass" style=​"background-color:​ green;​ padding:​ 1rem;​">​ First Para ​</p>​
+// fpara.classList.add
+// ƒ add() { [native code] }
+// fpara.classList.add('thirdclass')
+// undefined
+// fpara.classList
+// DOMTokenList(3) ['firstClass', 'secondClass', 'thirdclass', value: 'firstClass secondClass thirdclass']
+// fpara.classList.remove('thirdclass')
+// undefined
+// fpara.classList
+// DOMTokenList(2) ['firstClass', 'secondClass', value: 'firstClass secondClass']0: "firstClass"1: "secondClass"length: 2value: "firstClass secondClass"[[Prototype]]: DOMTokenList
+// fpara.classList.contains('thirdclass')
+// false
+// fpara.classList.contains('secondClass')
+// true
