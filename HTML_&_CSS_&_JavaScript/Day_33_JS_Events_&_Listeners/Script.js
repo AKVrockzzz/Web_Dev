135cc53e71265b5e7d6fbf6ae397b246283f9590
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2025-01-10 15:44:09 +0530

    event listener javascript
    event listener javascript

--- HTML_&_CSS_&_JavaScript/Day_33_JS_Events_&_Listeners/Script.js
+++ HTML_&_CSS_&_JavaScript/Day_33_JS_Events_&_Listeners/Script.js
@@ -2,11 +2,60 @@ console.log("Events & Listeners JavaScript");
 
 
 
-function changeText() {
-    let fpara = document.getElementById("fpara");
-    fpara.textContent = "Hello Ajay";
-}
+function alertPara(event){
+    if(event.target.nodeName = 'SPAN'){
+        alert("You have clicked on para: " + event.target.textContent);
 
-let fpara = document.getElementById("fpara");
-fpara.addEventListener('click', changeText);
+    }
+};
+
+let mydiv = document.getElementById('fdiv');
+document.addEventListener('click', alertPara);
+
+
+
+
+
+
+// let paras = document.querySelectorAll('p');
+
+
+// function alertPara(event){
+//     alert("You have clicked on para: " + event.target.textContent);
+// };
+// for(let i=0; paras.length; i++){
+//     let para = paras[i];
+//     para.addEventListener('click', alertPara);
+// }
+
+
+
+
+
+
+
+
+
+
+// let anchorElement = document.getElementById('fanchor');
+
+// anchorElement.addEventListener('click', function(event){
+//     event.preventDefault();
+//     anchorElement.textContent = "Click done bhai";
+
+// });
+
+
+
+
+
+
+// function changeText(event) {
+//     console.log(event);
+//     let fpara = document.getElementById("fpara");
+//     fpara.textContent = "Hello Ajay";
+// }
+
+// let fpara = document.getElementById("fpara");
+// fpara.addEventListener('click', changeText);
 // fpara.removeEventListener('click', changeText);
