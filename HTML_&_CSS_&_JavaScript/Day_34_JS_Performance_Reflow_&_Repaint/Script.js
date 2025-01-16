932b32c8dd63532b68e27ed389584ac6e5bbac93
Ajay Kumar Verma <122876823+AKVrockzzz@users.noreply.github.com>
2025-01-16 19:45:22 +0530

    JavaScript performance reflow & repaint
    JavaScript performance reflow & repaint

---
+++ HTML_&_CSS_&_JavaScript/Day_34_JS_Performance_Reflow_&_Repaint/Script.js
@@ -0,0 +1,46 @@
+
+
+
+// // COde 1
+// const t1 = performance.now();
+
+
+// for(let i = 1; i<= 100; i++){
+//     let para  = document.createElement('p');
+//     para.textContent = "This is Para " + i;
+//     document.body.appendChild(para);
+// }
+
+
+// const t2 = performance.now();
+// console.log("Total time taken by code 1 : " + (t2 - t1));
+
+
+
+// // COde 2
+// const t3 = performance.now();
+// let mydiv = document.createElement('div');
+
+// for(let i=1; i<=100; i++){
+//     let para  = document.createElement('p');
+//     para.textContent = "This is Para " + i;
+//     mydiv.appendChild(para); 
+// }
+
+// document.body.appendChild(mydiv);
+
+// const t4 = performance.now();
+// console.log("Total time taken by code 2 : " + (t4 - t3));
+
+
+// Best Code
+
+let fragment = document.createDocumentFragment();
+
+for(let i=1; i<=100; i++){
+    let para = document.createElement('p');
+    para.textContent = "This is para : " + i;
+    fragment.appendChild(para);
+}
+
+document.body.appendChild(fragment);
