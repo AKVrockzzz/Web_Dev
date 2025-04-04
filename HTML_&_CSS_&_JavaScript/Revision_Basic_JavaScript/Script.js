70cd1d3307225a7e588eecbd56a3a84c0ce08330
AKVrockzzz <ajaykumarverma5549@gmail.com>
2025-04-04 14:59:10 +0530

    Revision Basic topics of JavaScript
    In this code file i complete do practice on basic topics of javascript for revise all topics.

---
+++ HTML_&_CSS_&_JavaScript/Revision_Basic_JavaScript/Script.js
@@ -0,0 +1,103 @@
+console.log("Revision Of Basic JavaSCript");
+
+// condition Statement - if, else, elseif
+
+// if
+
+// if(10 < 9){
+//     console.log(true);
+// }
+// if(10 > 9){
+//     console.log(true);
+// }
+
+
+// if else
+
+// if(10 < 9){
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+
+
+// Loops - for & while
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+// let i = 10;
+// while(i < 21){
+//     console.log(i);
+//     i++;
+// }
+
+
+// Functions
+
+// function hellobolo(){
+//     console.log("Hello");
+// }
+
+// hellobolo();
+
+
+// function abcd(a,b,c){
+//     console.log(a,b,c);
+// }
+
+// abcd(12,13,14);
+
+
+//arrays
+// var arr = [11,15,32,12,65,44];
+// // console.log(arr[0]);
+
+// arr.push(10);
+// console.log(arr);
+
+// arr.pop();
+// console.log(arr);
+
+// arr.unshift(144);
+// console.log(arr);
+
+// arr.shift();
+// console.log(arr);
+
+// arr.splice(2,2);
+// console.log(arr);
+
+
+
+// Objects
+
+// 1) Black object 
+var a = {};
+
+// 2) Filled Object;
+
+var a = {
+    age : 24,
+    name : "Ajay",
+    email : "email@male.com",
+    number : 342342342342,
+};
+
+
+var ghadi = {
+    brand : "Rolax",
+    price : "100k",
+    color : "silver",
+    type : "digital",
+    automatic : false,
+};
+
+
+console.log(ghadi.type);
+
+ghadi.brand = "Titan";
+console.log(ghadi);
